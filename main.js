f0609003d2f491cc66f1f6f9cba2632c700ef61c
Robert051998 <96495255+Robert051998@users.noreply.github.com>
2024-06-09 18:57:55 +0200

    saving changes

--- main.js
+++ main.js
@@ -49,12 +49,4 @@ rightArrow.addEventListener('click', () => {
     }
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    const images = document.querySelectorAll('img');
-    images.forEach(img => {
-        img.setAttribute('loading', 'lazy');
-    });
-});
-
-
 
