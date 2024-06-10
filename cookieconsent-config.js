e8b96c529521e111ee3d429685a3632ba6e8e933
Robert051998 <96495255+Robert051998@users.noreply.github.com>
2024-06-10 18:54:05 +0200

    added cookie consent

---
+++ cookieconsent-config.js
@@ -0,0 +1,75 @@
+import 'https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@3.0.1/dist/cookieconsent.umd.js';
+
+CookieConsent.run({
+    guiOptions: {
+        consentModal: {
+            layout: "box",
+            position: "bottom left",
+            equalWeightButtons: true,
+            flipButtons: false
+        },
+        preferencesModal: {
+            layout: "box",
+            position: "right",
+            equalWeightButtons: true,
+            flipButtons: false
+        }
+    },
+    categories: {
+        necessary: {
+            readOnly: true
+        },
+        functionality: {},
+        analytics: {}
+    },
+    language: {
+        default: "en",
+        autoDetect: "browser",
+        translations: {
+            en: {
+                consentModal: {
+                    title: "Hello traveller, it's cookie time!",
+                    description: "We use cookies to enhance your browsing experience, serve personalized ads or content, and analyze our traffic. By clicking \"Accept All\", you consent to our use of cookies.",
+                    closeIconLabel: "",
+                    acceptAllBtn: "Accept all",
+                    acceptNecessaryBtn: "Reject all",
+                    showPreferencesBtn: "Manage preferences",
+                    footer: "<a href=\"https://vilalisjak.com/pages/privacy\">Privacy Policy</a>\n"
+                },
+                preferencesModal: {
+                    title: "Consent Preferences Center",
+                    closeIconLabel: "Close modal",
+                    acceptAllBtn: "Accept all",
+                    acceptNecessaryBtn: "Reject all",
+                    savePreferencesBtn: "Save preferences",
+                    serviceCounterLabel: "Service|Services",
+                    sections: [
+                        {
+                            title: "Cookie Usage",
+                            description: "We use cookies to enhance your browsing experience, serve personalized ads or content, and analyze our traffic. By clicking \"Accept All\", you consent to our use of cookies."
+                        },
+                        {
+                            title: "Strictly Necessary Cookies <span class=\"pm__badge\">Always Enabled</span>",
+                            description: "",
+                            linkedCategory: "necessary"
+                        },
+                        {
+                            title: "Functionality Cookies",
+                            description: "",
+                            linkedCategory: "functionality"
+                        },
+                        {
+                            title: "Analytics Cookies",
+                            description: "",
+                            linkedCategory: "analytics"
+                        },
+                        {
+                            title: "More information",
+                            description: "For any query in relation to our policy on cookies and your choices, please <a class=\"cc__link\" href=\"mailto:vila.lisjak@gmail.com\">contact us</a>."
+                        }
+                    ]
+                }
+            }
+        }
+    }
+});
